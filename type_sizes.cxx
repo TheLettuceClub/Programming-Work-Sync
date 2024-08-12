66a0f8b2bbd8c05b19c84aeb60ec2153db4c46e0
Dan <118778758+TheLettuceClub@users.noreply.github.com>
2024-08-12 14:03:29 -0500

    stuff

---
+++ type_sizes.cxx
@@ -0,0 +1,38 @@
+#include<iostream.h>
+using namespace std;
+int main(int argc, char *argv[])
+{
+	cout << "Fundamental types:" << endl;
+	cout << "char: " << sizeof(char) <<"\r\n";
+	cout << "short int: " << sizeof(short int) <<"\r\n";
+	cout << "int: " << sizeof(int) <<"\r\n";
+	cout << "long int: " << sizeof(long int) <<"\r\n";
+	cout << "float: " << sizeof(float) <<"\r\n";
+	cout << "double: " << sizeof(double) <<"\r\n";
+	cout << "long double: " << sizeof(long double) <<"\r\n";
+	cout << "unsigned char: " << sizeof(unsigned char) <<"\r\n";
+	cout << "unsigned short int: " << sizeof(unsigned short int) <<"\r\n";
+	cout << "unsigned int: " << sizeof(unsigned int) <<"\r\n";
+	cout << "unsigned long int: " << sizeof(unsigned long int) <<"\r\n";
+	cout << "signed char: " << sizeof(signed char) <<"\r\n";
+	cout << "signed short int: " << sizeof(signed short int) <<"\r\n";
+	cout << "signed int: " << sizeof(signed int) <<"\r\n";
+	cout << "signed long int: " << sizeof(signed long int) <<endl;
+	
+	cout << "Pointer types:" << endl;
+	cout << "char*: " << sizeof(char*) <<"\r\n";
+	cout << "short int*: " << sizeof(short int*) <<"\r\n";
+	cout << "int*: " << sizeof(int*) <<"\r\n";
+	cout << "long int*: " << sizeof(long int*) <<"\r\n";
+	cout << "float*: " << sizeof(float*) <<"\r\n";
+	cout << "double*: " << sizeof(double*) <<"\r\n";
+	cout << "long double*: " << sizeof(long double*) <<"\r\n";
+	cout << "unsigned char*: " << sizeof(unsigned char*) <<"\r\n";
+	cout << "unsigned short int*: " << sizeof(unsigned short int*) <<"\r\n";
+	cout << "unsigned int*: " << sizeof(unsigned int*) <<"\r\n";
+	cout << "unsigned long int*: " << sizeof(unsigned long int*) <<"\r\n";
+	cout << "signed char*: " << sizeof(signed char*) <<"\r\n";
+	cout << "signed short int*: " << sizeof(signed short int*) <<"\r\n";
+	cout << "signed int*: " << sizeof(signed int*) <<"\r\n";
+	cout << "signed long int*: " << sizeof(signed long int*) <<endl;
+}
